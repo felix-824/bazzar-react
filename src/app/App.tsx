4c50ad9965b24507f68af9c6986640ac76618703
felix-824 <nf94new@gmail.com>
2026-08-31 03:49:38 +0900

    feat: set up Redux store and frontend base

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Bazzar</h1>
+      <p>Fresh groceries delivered to your door.</p>
+    </div>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './app/App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <Provider store={store}>
+    <App/>
+   </Provider>
+    
   </StrictMode>,
 )
